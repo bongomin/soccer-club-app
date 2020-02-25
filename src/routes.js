75a2edf3968595534bff1bd4dbd67aa81a083fa5
bongomin <candanobongomindaniel@gmail.com>
2020-02-25 16:39:46 +0300

    feat(Home component):
    - created a home folder and placed in featured folder
    
    - inside the featured created the stripe component which handles the slid and animation
    
    - created the home index which is rendered in the routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Layout from './Components /HOC/Layout';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Components /home';
 
 const Routes = (props) => {
   return (
-    <Layout>
-      second Line
-    </Layout>
-  )
 
+    <Layout>
+      <Switch>
+        <Route exact component={Home} path="/" />
+      </Switch>
+    </Layout>
+
+  )
 }
 
 export default Routes;
